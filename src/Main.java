8bb685916c3c427a8469541f67c160c18b84405c
Guilherme <guilhermeghisi14@gmail.com>
2023-02-27 17:13:23 -0300

    Fazendo o Cálculo

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         File file = new File("src/array.txt");
-
+        int intervalo = 4;
         ArrayList<Double> valores = new ArrayList<>();
 
         try {
@@ -20,11 +20,34 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Arquivo não encontrado" + e.getMessage());
         }
+
+        ArrayList<Double> menoresDistancias = new ArrayList<>();
         for (int i = 0; i < valores.size(); i++) {
-            System.out.println(valores.get(i));
+            if (i + intervalo > valores.size())
+                break;
+            ArrayList<Double> subArray = new ArrayList<>(valores.subList(i, i + intervalo));
+            double distanciaMinima = Double.MAX_VALUE;
+            for (int j = 0; j < valores.size(); j++) {
+                if (j + intervalo > valores.size())
+                    break;
+                ArrayList<Double> subArray2 = new ArrayList<>(valores.subList(j, j + intervalo));
+                double calculo = 0;
+                for (int k = 0; k < intervalo; k++) {
+                    calculo += Math.pow(subArray.get(k) - subArray2.get(k), 2);
+
+                }
+                calculo = Math.sqrt(calculo);
+                if (calculo < distanciaMinima && calculo != 0) {
+                    distanciaMinima = calculo;
+                }
+                System.out.println("Array2" + subArray2);
+            }
+            menoresDistancias.add(distanciaMinima);
+            System.out.println("Array1" + subArray);
+
         }
-
-
+        System.out.println(menoresDistancias);
     }
 
 }
+
