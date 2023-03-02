78bf529e4fa3f9ace049e27b1419be0692fb33cd
Guilherme <guilhermeghisi14@gmail.com>
2023-03-02 15:07:11 -0300

    Colocando como args

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/array.txt");
-        int intervalo = 4;
+        File file = new File(args[0]);
+        int intervalo = Integer.parseInt(args[1]);
         ArrayList<Double> valores = new ArrayList<>();
         long start = System.currentTimeMillis();
         try {
